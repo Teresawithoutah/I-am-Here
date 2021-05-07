d1374dbc26e610bca2ab18df9d86ba3038a75959
Teresa M <teresawithoutah@gmail.com>
2021-05-07 13:50:29 -0400

    1st round functional
    open meeting & close meeting works w user input but while loop is funky.

--- iamhere.sh
+++ iamhere.sh
@@ -1,13 +1,24 @@
+
 # ask user input for meeting code
 read -p "Enter your meeting code: " meetingnumber
 echo "Your meeting code: $meetingnumber"
 
+read -p "Enter when your class ends in the format 00:00 " endclass
+echo "We will close your meeting at $endclass"
 
 # ask user when class starts and ends
 #read -p "Enter when your class starts: " startclass
-#read -p "Enter when your class ends: "  endclass
-
+#echo "We will close your meeting at $endclass"
 
 open "zoommtg://zoom.us/join?confno=$meetingnumber"
 
-#pkill zoom
+while true
+do
+  NOW=$(date +"%H:%M")
+  echo "Waiting for class to end..."
+  if [ $NOW = $endclass ]
+  then
+    #ps aux | grep -i zoom
+    pkill zoom
+  fi
+done
